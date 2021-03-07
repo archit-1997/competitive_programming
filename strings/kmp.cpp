2d60158a3ab0b86aa82670bdcdae625b04e89986
Archit Singh <architsingh456@gmail.com>
2021-03-07 20:23:57 +0530

    added code for kmp algorithm in strings

---
+++ strings/kmp.cpp
@@ -0,0 +1,34 @@
+//prefix array of the kmp algorithm
+//this is O(n3) complexity
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s;cin>>s;
+    int n=s.size();
+
+    vector<int> pi(n,0);
+
+    //we know that by definition, pi[0]=0;
+
+    for(int i=1;i<n;i++){
+        string str=s.substr(0,i+1);
+        //this string is of length i+1
+        int len=i+1;
+        //j indicates the length of the substring
+        for(int j=1;j<=i+1;j++){
+           string pre=str.substr(0,j);
+           string suff=str.substr(len-j); 
+           if(pre==suff)
+            pi[i]=len;
+        }
+    }
+
+    for(int i=0;i<n;i++)
+        cout<<pi[i]<<" ";
+
+
+    return 0;
+}
---
+++ strings/kmp_1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main(){
+
+    string s;cin>>s;
+    int n=s.size();
+
+    vector<int> pi(n,0);
+
+    //by definition, pi[0]=0
+    int i,j;
+    for(i=1;i<n;i++){
+        j=pi[i-1];
+        while(j>0 && s[i]!=s[j])
+        //this statement will take us to the previous length of the longest common prefix which was also a suffix
+            j=pi[j-1];
+        if(s[i]==s[j])
+            j++;
+        pi[i]=j;
+    }
+
+    return 0;
+}
